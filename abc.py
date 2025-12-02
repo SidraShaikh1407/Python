9c385a08c940c85722485faa05cb71afb35a2321
Sidra Shaikh <sidrashaikh8455@gmail.com>
2025-12-02 14:41:40 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+from pack1 import *
+
+x=Inheritance1.shape(10,15)
+print(x.area())
+print(x.perimeter())
+
+y=Inheritance1.box(10,15,20)
+print(y.volume())
+print(y.perimeter())
+
+p=primeno.prime(1,20)
+k=p.getprime()
+print(k)
